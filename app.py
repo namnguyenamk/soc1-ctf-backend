99020c5aeb1f2d7893978f7109c0961c20e1dde4
namnguyenamk <namnguyenamk@gmail.com>
2025-12-05 20:43:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,23 +20,23 @@ questions = {
         "answer": "209.35.99.117"
     },
     2: {
-        "question": "Question2— Firewall Log Analysis.In 'bots1_firewall_log'.What is the:➡ **destination.ip** of the session **denied by the firewall (action = DENY)**?
-👉 Enter the answer in the form:`192.168.xxx.xxx`",
+        "question": "Question2: Firewall Log Analysis.In bots1_firewall_log.What is the: destination.ip of the session denied by the firewall (action = DENY)?
+👉 Enter the answer in the form:192.168.xxx.xxx",
         "answer": "ANSWER_Q2"
     },
     3: {
-        "question": "Question 3 — Firewall Log Analysis: In 'bots1_firewall_log', determine:➡ Which **destination port** is blocked the most by the **firewall**?
-👉 Please provide the **destination.port** of this event.",
+        "question": "Question3: Firewall Log Analysis: In bots1_firewall_log, determine: Which destination port is blocked the most by the firewall?
+👉 Please provide the destination.port of this event",
         "answer": "ANSWER_Q3"
     },
     4: {
-        "question": "Question4: In the `bots1_firewall_log`.Firewall Behavior, find:➡ Which **Source IP** was **rejected the most times** by the firewall?
-👉 Please provide the **source.ip** of this event",
+        "question": "Question4: In the bots1_firewall_log.Firewall Behavior, Let's find: Which Source IP was rejected the most times by the firewall?
+👉 Please provide the source.ip of this event",
         "answer": "ANSWER_Q4"
     },
     5: {
-        "question": "Question 5 — Protocol Inspection.Based on **bots1_firewall_log**, determine:➡ Which **protocol** appears in the **DENY** logs?
-👉 Please provide the **protocal** of this event.",
+        "question": "Question5: Protocol Inspection.Based on bots1_firewall_log, determine: Which protocol appears in the DENY logs?
+👉 Please provide the protocal of this event",
         "answer": "ANSWER_Q5"
     }
 }
